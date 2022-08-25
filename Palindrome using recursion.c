88bcc69cc0e03e1a139c2e83c1d52e12106a6faa
samashrestha <shrsama67@gmail.com>
2022-08-25 21:32:20 +0545

    added

--- Palindrome using recursion.c
+++ Palindrome using recursion.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-
+#include<conio.h>
+#include<math.h>
 int reverse(int);
-
-int main(){
-
+int main()
+{
     int numbr,sum;
 
     printf("Enter a number: ");
@@ -13,35 +13,30 @@ int main(){
     sum = reverse(numbr);
 
     if(numbr==sum)
-
+	{
          printf("The given number  %d is a palindrome",numbr);
-
+	}
     else
-
+	{
     printf("The given number  %d is not a palindrome",numbr);
-
+	}
     return 0;
-
 }
 
-int reverse(int num){
+int reverse(int num)
+{
 
     int rem;
 
-static int sum=0;
+	static int sum=0;
 
-    if(num!=0){
-
-         rem=num%10;
-
-         sum=sum*10+rem;
-
-         reverse(num/10);
-
+    if(num!=0)
+	{
+        rem=num%10;
+	    sum=sum*10+rem;
+        reverse(num/10);
     }
-
     return(sum);
-
 }
 
  
